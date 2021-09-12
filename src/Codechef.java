05554d3f04db4982140755af5fa48208331363bb
prajjwalkumar17 <bob@example.com>
2021-09-12 16:07:56 +0530

    Travel problem complete Rishabh bhaiya code

--- src/Codechef.java
+++ src/Codechef.java
@@ -1,93 +1,18 @@
-/* package codechef; // don't place package name! */
 
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
 class Codechef
 {
-    private static Scanner scanner=new Scanner(System.in);
-
     public static void main (String[] args) throws java.lang.Exception
     {
-        try {
-            int testcases = scanner.nextInt();
-            int[] result = new int[testcases];
-            if (testcases == 1) {
 
-
-                int N = scanner.nextInt();
-
-                int A = scanner.nextInt();
-
-                int B = scanner.nextInt();
-                String S = scanner.next();
-                char[] Schar = new char[String.valueOf(N).length()];
-                Schar = S.toCharArray();
-                int counttimeforinterdistrict = 0;
-                int countimeforinterstate = 0;
-                for (int tt = 0; tt < N; tt++) {
-                    int xr = Integer.parseInt(String.valueOf(Schar[tt]));
-                    if (xr == 0) {
-                        counttimeforinterdistrict++;
-                    } else if (xr == 1) {
-                        countimeforinterstate++;
-                    }
-                }
-                System.out.println(A * counttimeforinterdistrict + B * countimeforinterstate);
-            }
-
-            else {
-                for (int a = 0; a < testcases; a++) {
-                    int N = scanner.nextInt();
-
-                    int A = scanner.nextInt();
-
-                    int B = scanner.nextInt();
-
-                    result[a] = printyes(N, A, B, testcases);
-                }
-                for (int ui = 0; ui < testcases; ui++) {
-                    System.out.println(result[ui]);
-
-                }
-            }
-        }catch(Exception e){
-            System.out.println(e.getMessage());
-            return;
-
-        }
-        // your code goes here
     }
-    private static int printyes(int N, int A, int B, int testcases) throws Exception
-    {
-        Scanner scanner=new Scanner(System.in);
-        int Result=0;
-        try {
-            for (int a = 0; a < testcases / 2; a++) {
-//            System.out.println(x+" "+y+" "+z);
-//            System.out.println("enter the different X  values");
-                String S = scanner.next();
-                char[] Schar = new char[String.valueOf(N).length()];
-                Schar = S.toCharArray();
-                int counttimeforinterdistrict = 0;
-                int countimeforinterstate = 0;
-                for (int tt = 0; tt < N; tt++) {
-                    int xr = Integer.parseInt(String.valueOf(Schar[tt]));
-                    if (xr == 0) {
-                        counttimeforinterdistrict++;
-                    } else if (xr == 1) {
-                        countimeforinterstate++;
-                    }
-                }
-                Result = A * counttimeforinterdistrict + B * countimeforinterstate;
-                return Result;
-            }
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-        return Result;
-    }
-
 }
+
+
+
+
+
+
--- src/TravelPass.java
+++ src/TravelPass.java
@@ -2,9 +2,33 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class TravelPass {
-    public static void main(String[] args)
-    {
-        Scanner scanner=new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+        while (test != 0) {
+            int n = sc.nextInt();
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            String str = sc.next();
+            int count_zero = 0;
+            int count_one = 0;
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == '0') {
+                    count_zero++;
+                } else {
+                    count_one++;
+                }
+            }
+            int time = (count_zero * a) + (count_one * b);
+            System.out.println(time);
+            test--;
+
+        }
+    }
+}
+  /*      Scanner scanner=new Scanner(System.in);
         int testcases = scanner.nextInt();
         int[] result = new int[testcases];
         if (testcases == 1) {
@@ -49,9 +73,10 @@ public class TravelPass {
                 System.out.println(result[ui]);
 
             }
-        }
-    }
+        }*/
 
+
+/*
     private static int printyes(int x, int y, int z, int testcases) {
         Scanner scanner=new Scanner(System.in);
         int yuyu=0;
@@ -83,5 +108,6 @@ public class TravelPass {
         }
         return yuyu;
     }
-}
+*/
+
 
