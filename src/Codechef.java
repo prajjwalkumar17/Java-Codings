35cc77d7075b7246ec49ab07da3df195dc4a483c
prajjwalkumar17 <talk2prajjwal@gmail.com>
2021-09-13 23:58:01 +0530

    airline and Travel case completed
    Signed-off-by: prajjwalkumar17 <bob@example.com>

--- src/Codechef.java
+++ src/Codechef.java
@@ -10,25 +10,16 @@ class Codechef
         Scanner sc=new Scanner(System.in);
         int test=sc.nextInt();
         while(test!=0){
-            int A=sc.nextInt();
-            int B=sc.nextInt();
+            int N=sc.nextInt();
 
-            int C=sc.nextInt();
+            int X=sc.nextInt();
 
-            int D=sc.nextInt();
-
-            int E=sc.nextInt();
-
-            int max=Math.max(Math.max(A,C),B);
-            int min=Math.min(Math.min(A,B),C);
-
-            int thirdno=A+B+C-(max+min);
-
-            if(min<=E && max+thirdno<=D || max+min<=D && thirdno<=E || min+thirdno<=D && max<=E){
-                System.out.println("YES");
-            } else {
-                System.out.println("NO");
+            int[] array = new int[N];
+            for(int i=0;i<N;i++){
+                array[i]=sc.nextInt();
             }
+
+            System.out.println(N+" "+X+" "+array[1]);
             test--;
         }
     }
