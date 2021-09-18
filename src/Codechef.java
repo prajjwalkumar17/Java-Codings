0b273b0fc616df9a1e4361e94f6e31352e3db5b5
prajjwalkumar17 <talk2prajjwal@gmail.com>
2021-09-18 19:13:35 +0530

    dishes array sum find index and and direction matrix arrray solved
    Signed-off-by: prajjwalkumar17 <bob@example.com>

--- src/Codechef.java
+++ src/Codechef.java
@@ -5,41 +5,9 @@ import java.io.*;
 
 class Codechef {
     public static void main(String[] args) throws java.lang.Exception {
-        Scanner sc = new Scanner(System.in);
-        int test = sc.nextInt();
-        while (test != 0) {
-            int N = sc.nextInt();
-            int[][] A = new int[N][N];
-            if (N % 2 == 0) {
-                for (int i = 0; i < N; i++) {
-                    for (int j = 0; j < N; j++) {
-                        A[i][j] = -1;
-                    }
-                }
-            } else {
-                for (int i = 0; i < N; i++) {
-                    for (int j = 0; j < N; j++) {
-                        if (i % 2 != 0 || j % 2 != 0) {
-                            A[i][j] = 1;
-                        } else {
-                            A[i][j] = -1;
-                        }
-                    }
-                }
 
 
-            }
-            for (int y = 0; y < N; y++) {
-                for (int z = 0; z < N; z++) {
-                    System.out.print(A[y][z]);
-                    System.out.print(" ");
-                }
-                System.out.println();
-            }
-            test--;
-        }
-
-
+        
     }
 
 
