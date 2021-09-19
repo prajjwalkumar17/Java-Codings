c0163186b668d57cca6a38af1db4b08045d8b23b
prajjwalkumar17 <talk2prajjwal@gmail.com>
2021-09-20 00:15:32 +0530

    cookoff19sep
    Signed-off-by: prajjwalkumar17 <bob@example.com>

--- src/Codechef.java
+++ src/Codechef.java
@@ -3,9 +3,6 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-
-
-
 class Codechef {
     public static void main(String[] args) throws java.lang.Exception {
 
@@ -13,20 +10,48 @@ class Codechef {
         int test = sc.nextInt();
         while (test != 0) {
 
-            int N = sc.nextInt();
+            int noofblocks = sc.nextInt();
+            int noofjumps = sc.nextInt();
+            String S = sc.next();
+            char[] blocks = S.toCharArray();
 
-
+                alternateblockcounter(blocks, noofblocks, noofjumps);
 
 
             test--;
         }
     }
 
+    private static void alternateblockcounter(char[] blocks, int noofblocks, int noofjumps) {
+        if (noofjumps > 0 && blocks[noofblocks - 1] != blocks[0]) {
+            System.out.println(noofblocks);
+        } else {
+            int intjump = noofjumps;
+            int result = 0;
+            while (noofjumps != 0) {
+                int p = 0;
+                for (int i = 1; i < noofblocks; i++) {
+                    if (blocks[i] != blocks[p]) {
+                        p = i;
+                    }
+                }
+                result = p + 1;
+                noofjumps--;
+            }
+            if (result == 1 || result == 0) {
+                System.out.println(-1);
+            } else {
+                if (intjump == 1) {
+                    System.out.println(result - 1);
+                } else {
+                    System.out.println(result);
+                }
+            }
+        }
+    }
+
 
 }
-
-
-
 
 
 
