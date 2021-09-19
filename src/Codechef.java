1e1186fd2c8b92db78416a0398ef156f11026618
prajjwalkumar17 <talk2prajjwal@gmail.com>
2021-09-20 00:39:24 +0530

    cookoff19sep
    Signed-off-by: prajjwalkumar17 <bob@example.com>

--- src/Codechef.java
+++ src/Codechef.java
@@ -10,48 +10,18 @@ class Codechef {
         int test = sc.nextInt();
         while (test != 0) {
 
-            int noofblocks = sc.nextInt();
-            int noofjumps = sc.nextInt();
-            String S = sc.next();
-            char[] blocks = S.toCharArray();
-
-                alternateblockcounter(blocks, noofblocks, noofjumps);
-
-
+            int n= sc.nextInt();
+            for(int i=n;i>0;i--){
+                System.out.print(i+" ");
+            }
             test--;
         }
     }
 
-    private static void alternateblockcounter(char[] blocks, int noofblocks, int noofjumps) {
-        if (noofjumps > 0 && blocks[noofblocks - 1] != blocks[0]) {
-            System.out.println(noofblocks);
-        } else {
-            int intjump = noofjumps;
-            int result = 0;
-            while (noofjumps != 0) {
-                int p = 0;
-                for (int i = 1; i < noofblocks; i++) {
-                    if (blocks[i] != blocks[p]) {
-                        p = i;
-                    }
-                }
-                result = p + 1;
-                noofjumps--;
-            }
-            if (result == 1 || result == 0) {
-                System.out.println(-1);
-            } else {
-                if (intjump == 1) {
-                    System.out.println(result - 1);
-                } else {
-                    System.out.println(result);
-                }
-            }
-        }
-    }
 
 
 }
+
 
 
 
--- src/Zebracrossingprob19cook.java
+++ src/Zebracrossingprob19cook.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 /**
  * There's a zebra crossing appearing in the middle of nowhere with N
@@ -56,7 +57,54 @@
 
 
 public class Zebracrossingprob19cook {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws java.lang.Exception {
 
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+        while (test != 0) {
+
+            int noofblocks = sc.nextInt();
+            int noofjumps = sc.nextInt();
+            String S = sc.next();
+            char[] blocks = S.toCharArray();
+
+            alternateblockcounter(blocks, noofblocks, noofjumps);
+
+
+            test--;
+        }
     }
+
+    private static void alternateblockcounter(char[] blocks, int noofblocks, int noofjumps) {
+        if (noofjumps > 0 && blocks[noofblocks - 1] != blocks[0]) {
+            System.out.println(noofblocks);
+        } else {
+            int intjump = noofjumps;
+            int result = 0;
+            while (noofjumps != 0) {
+                int p = 0;
+                for (int i = 1; i < noofblocks; i++) {
+                    if (blocks[i] != blocks[p]) {
+                        p = i;
+                    }
+                }
+                result = p + 1;
+                noofjumps--;
+            }
+            if (result == 1 || result == 0) {
+                System.out.println(-1);
+            } else {
+                if (intjump == 1) {
+                    System.out.println(result - 1);
+                } else {
+                    System.out.println(result);
+                }
+            }
+        }
+    }
+
+
 }
+
+
+
