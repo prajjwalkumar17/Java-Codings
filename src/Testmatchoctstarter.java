84efb4c7d65d1349302f37c2217b7d6dcc747bf4
prajjwalkumar17 <talk2prajjwal@gmail.com>
2021-10-19 01:30:08 +0530

    oct starter

--- src/Testmatchoctstarter.java
+++ src/Testmatchoctstarter.java
@@ -1,19 +1,15 @@
+import java.util.Scanner;
 
-import java.lang.reflect.Array;
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-class Codechef {
-/**
- * A 5 match test series between India and England has just concluded.
- *
- * Every match could have ended either as a win for India, a win for England, or a draw. You know the result of all the matches. Determine who won the series or if it ended in a draw.
- *
- * A team is said to have won the series if it wins strictly more test matches than the other team.
- * Ri=0  denotes that the test match ends in a draw.
- * Ri=1 denotes that the test match is won by India.
- * Ri=2 denotes that the test match is won by England.*/
+public class Testmatchoctstarter {
+    /**
+     * A 5 match test series between India and England has just concluded.
+     *
+     * Every match could have ended either as a win for India, a win for England, or a draw. You know the result of all the matches. Determine who won the series or if it ended in a draw.
+     *
+     * A team is said to have won the series if it wins strictly more test matches than the other team.
+     * Ri=0  denotes that the test match ends in a draw.
+     * Ri=1 denotes that the test match is won by India.
+     * Ri=2 denotes that the test match is won by England.*/
     public static void main(String[] args) throws java.lang.Exception {
         Scanner sc = new Scanner(System.in);
         int test = sc.nextInt();
