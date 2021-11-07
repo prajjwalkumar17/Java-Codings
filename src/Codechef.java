583214c3da9b23c7fe92139ea3da28ce2d8b2e43
prajjwalkumar17 <talk2prajjwal@gmail.com>
2021-11-07 14:22:58 +0530

    nov long challenge

--- src/Codechef.java
+++ src/Codechef.java
@@ -4,22 +4,29 @@ import java.lang.*;
 
 class Codechef {
     /**
-     Faizal is very sad after finding out that he is responsible for Sardar's death. He tries to drown his sorrows in alcohol and gets very drunk. Now he wants to return home but is unable to walk straight. For every 3 steps forward, he ends up walking one step backward.
-
-     Formally, in the 1st second he moves 3 steps forward, in the 2nd second he moves 1 step backwards, in the 3rd second he moves 3 steps forward, in 4th second 1 step backwards, and so on.
-
-     How far from his initial position will Faizal be after k seconds have passed? Assume that Faizal's initial position is 0.
+     * The current price of petrol is X rupees, and the current price of diesel is Y rupees. At the start of each month, the price
+     * of petrol increases by A rupees and the price of diesel increases by B rupees.
+     * <p>
+     * Chef is curious to know which fuel costs less at the end of the Kth month. If petrol is cheaper than diesel at the end of Kth month,
+     * then print PETROL. If diesel is cheaper than petrol at the end of the Kth month, then print DIESEL. If both the fuels have the same price at the end of the Kth month,
+     * then print SAME PRICE.
      */
     public static void main(String[] args) throws java.lang.Exception {
         Scanner sc = new Scanner(System.in);
         int test = sc.nextInt();
         while (test-- > 0) {
-            int Time = sc.nextInt();
-            if(Time%2==0){
-                System.out.println(((Time/2)*3)-(Time/2));
-            } else{
-                System.out.println(((Time/2)*3)-(Time/2)+3);
+            int CurrentPetrol = sc.nextInt();
+            int CurrentDeisel = sc.nextInt();
+            int petrolinc = sc.nextInt();
+            int Deiselinc = sc.nextInt();
+            int noofmonths = sc.nextInt();
 
+            if ((CurrentPetrol + (petrolinc * noofmonths)) < (CurrentDeisel + (Deiselinc * noofmonths))) {
+                System.out.println("PETROL");
+            } else if ((CurrentPetrol + (petrolinc * noofmonths)) > (CurrentDeisel + (Deiselinc * noofmonths))) {
+                System.out.println("DIESEL");
+            } else {
+                System.out.println("SAME PRICE");
             }
 
         }
